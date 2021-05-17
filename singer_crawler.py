1aaba13cd43f23c236f0c5033915470a81b8ba4b
SungheeJin1 <shappy1120@gmail.com>
2021-05-17 20:08:20 +0900

    graph title added by SungheeJin1

--- singer_crawler.py
+++ singer_crawler.py
@@ -6,7 +6,7 @@ import numpy as np
 
 print("가수 이름 5개를 입력하시오(콤마로 구분할 것)")
 print('ex : 아이유,성시경,오마이걸,SG워너비,박재정')
-singeranswer=input(">> : ")
+singeranswer=input(">>")
 singerlist=singeranswer.split(",") #그래프 그릴 때 x축에 활용
 
 URL = 'https://www.genie.co.kr/search/searchMain?query=%25EA%25B0%2580%25EC%2588%2598'
@@ -39,6 +39,21 @@ for singer in singerlist:
     searchwindow = driver.find_element_by_xpath("""//*[@id="sc-fd"]""")
     searchwindow.clear()
 
+songnum1=[]
+for n in songnum:
+    new = ''
+    for i in n:
+        if i.isalnum():
+            new+=i
+        else:
+            continue
+    songnum1.append(new)
+
+songnum2=[]
+for i in songnum1:
+    t=float(i)
+    songnum2.append(t)
+
 likes1=[]
 for n in likes:
     new = ''
@@ -59,10 +74,20 @@ time.sleep(2)
 driver.close()
 
 print(singerlist)
-print(songnum)
+print(songnum2)
 print(likes2)
 
+x1 = np.arange(5)
+plt.figure(1)
+plt.rc('font', family='Malgun Gothic')
+plt.suptitle('가수 별 곡 건수')
+plt.bar(x1,songnum2)
+plt.xticks(x1, singerlist)
+plt.show()
+
 x=np.arange(5)
+plt.figure(2)
+plt.suptitle('가수 별 좋아요 수')
 plt.rc('font', family='Malgun Gothic')
 plt.bar(x, likes2)
 plt.xticks(x, singerlist)
