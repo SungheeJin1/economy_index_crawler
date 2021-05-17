8852b8b82870ee04ee647b3da7f3d72d35e7cddd
SungheeJin1 <shappy1120@gmail.com>
2021-05-17 14:40:43 +0900

    edited by SungheeJin1

--- singer_crawler.py
+++ singer_crawler.py
@@ -2,12 +2,17 @@ import time
 import selenium
 from selenium import webdriver
 
-list = input(print("가수 이름 5개를 입력하시오(콤마로 구분할 것):"))
-singerlist=list.split(",") #그래프 그릴 때 x축에 활용
+print("가수 이름 5개를 입력하시오(콤마로 구분할 것)")
+print('ex : 아이유,성시경,오마이걸,SG워너비,박재정')
+singeranswer=input(">> : ")
+singerlist=singeranswer.split(",") #그래프 그릴 때 x축에 활용
 
 URL = 'https://www.genie.co.kr/search/searchMain?query=%25EA%25B0%2580%25EC%2588%2598'
 driver = webdriver.Chrome(executable_path='./chromedriver.exe')
 driver.get(url=URL)
+
+songnum = []
+likes = []
 
 for singer in singerlist:
     searchwindow = driver.find_element_by_xpath("""//*[@id="sc-fd"]""")
@@ -19,16 +24,22 @@ for singer in singerlist:
     magbtn=driver.find_element_by_class_name("btn-submit")
     magbtn.click()
 
-    songs = driver.find_element_by_class_name("article").text
-    print(singer,songs)
+    songs = driver.find_element_by_xpath("""//*[@id="body-content"]/div[4]/div[1]/span/strong""").text
+    # print(singer,songs)
+    songnum.append(songs)
 
-    time.sleep(3)
+    like = driver.find_element_by_xpath("""/html/body/div[3]/div[2]/div/div[1]/div[3]/div/div[2]/span/a[3]/em""").text
+    # print(like)
+    likes.append(like)
+
+    time.sleep(2)
     searchwindow = driver.find_element_by_xpath("""//*[@id="sc-fd"]""")
     searchwindow.clear()
 
-    like = driver.find_element_by_xpath("""/html/body/div[3]/div[2]/div/div[1]/div[3]/div/div[2]/span/a[3]/em""").text
-    print(like)
-
-time.sleep(3)
+time.sleep(2)
 
 driver.close()
+
+print(singerlist)
+print(songnum)
+print(likes)
